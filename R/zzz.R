2ecf91d5f746a7e6d5f9fe35a7642aab58918115
Diogo <diogoferrari@gmail.com>
2020-03-03 10:21:41 -0600

    First commit

---
+++ R/zzz.R
@@ -0,0 +1,28 @@
+#' @importFrom magrittr %>%
+
+.onAttach<- function(libname, pkgname) 
+{
+ packageStartupMessage('
+
+ ## memisc2 package
+
+ ')
+} 
+
+.onAttach<- function(libname, pkgname) 
+{
+ packageStartupMessage('
+
+ ## memisc2 package
+
+ ')
+} 
+
+.onAttach<- function(libname, pkgname) 
+{
+ packageStartupMessage('
+
+ ## memisc2 package
+
+ ')
+} 
